d97ca86223eb5b9ce93f92df27396bbf0ee46996
Anmol Bansal <abanmolbansal5@gmail.com>
2023-01-15 16:22:15 +0530

    final

---
+++ README.md
@@ -0,0 +1,14 @@
+# Cogoport Academy - Problem set 1 - HTML & CSS Basic Website
+
+### Replica Complete
+
+#### Visit the Website [here](https://anmolbansal7.github.io/cogoport-a1-html-css/)
+
+- Score and Lives Remaining
+- Shows in Red when Last Life
+- Red Brick deducts 2 Points
+- Green Brick gives 5 Points
+- Game Over and Wohoo! Text
+- Paddle and Brick Colors
+
+#### Screenshot👇
--- sketch.js
+++ sketch.js
@@ -1,5 +1,20 @@
+//shows in red when last live remains
+//score in green when last brick remains
+//game over and wohoo alerts
+//red brick deducts 2 points
+//green brick gives 5 points
+//colored the paddle and bricks
+
+
 let paddle_x, paddle_y, paddle_width, paddle_height, paddle_dx;
 let ball_x, ball_y, ball_diameter, ball_dx, ball_dy;
+
+let brickRowCount, brickColCount, brickWidth, brickHeight, brickPadding;
+const bricks = [];
+
+let score = 0;
+let lives = 3;
+let brickCount = 14;
 
 function setup() {
 	createCanvas(400, 400);
@@ -9,11 +24,6 @@ function setup() {
 	paddle_y = height - 25;
 	paddle_height = 15;
 
-	brick_width = 45;
-	brick_x = 240;
-	brick_y = 100;
-	brick_height = 20;
-
 	ball_diameter = 20;
 	ball_dx = 1; //1
 	ball_dy = 2; //2
@@ -22,6 +32,23 @@ function setup() {
 
 	ball_x = width / 2 - ball_diameter / 2;
 	ball_y = height / 2 - ball_diameter / 2;
+
+	brickWidth = 50;
+	brickRowCount = 3;
+	brickColCount = 5;
+	brickPadding = 20;
+	brickHeight = 20;
+	for (let c = 0; c < brickColCount; c++) {
+		bricks[c] = [];
+		for (let r = 0; r < brickRowCount; r++) {
+			bricks[c][r] = {
+				x: 0,
+				y: 0,
+				status: true,
+				brickColor: "chocolate",
+			};
+		}
+	}
 }
 
 function draw() {
@@ -34,8 +61,14 @@ function draw() {
 
 	//game over on hitting floor
 	if (ball_y + ball_diameter / 2 > height) {
-		ball_dy = 0;
-		ball_dx = 0;
+		if (lives > 0) {
+			lives -= 1;
+			ball_dy = -ball_dy;
+		}
+		if (lives == 0) {
+			ball_dy = 0;
+			ball_dx = 0;
+		}
 	}
 
 	//bounce on hitting roof
@@ -49,10 +82,14 @@ function draw() {
 
 	//moving the PADDLE
 	if (keyIsDown(LEFT_ARROW)) {
-		paddle_x = paddle_x - paddle_dx;
+		if (paddle_x > 0) {
+			paddle_x = paddle_x - paddle_dx;
+		}
 	}
 	if (keyIsDown(RIGHT_ARROW)) {
-		paddle_x = paddle_x + paddle_dx;
+		if (paddle_x + paddle_width < width) {
+			paddle_x = paddle_x + paddle_dx;
+		}
 	}
 
 	//bounce back on hitting the paddle
@@ -65,18 +102,85 @@ function draw() {
 		ball_dy = -ball_dy;
 	}
 
-	//break brick on hitting
-	if (
-		ball_y + ball_diameter / 2 >= brick_y &&
-		ball_y + ball_diameter / 2 <= brick_y + brick_height &&
-		ball_x >= brick_x &&
-		ball_x <= brick_x + brick_width
-	) {
-		brick_width = 0;
-		//console.log("brick is hit");
+	circle(ball_x, ball_y, ball_diameter);
+	fill("grey");
+	rect(paddle_x, paddle_y, paddle_width, paddle_height);
+	fill(255, 255, 255);
+	// if (brick_status == true) {
+	// 	rect(brick_x, brick_y, brick_width, brick_height);
+	// };
+	for (let c = 0; c < brickColCount; c++) {
+		for (let r = 0; r < brickRowCount; r++) {
+			//defining the brick matrix
+			let brick_x = c * (brickWidth + brickPadding) + 30;
+			let brick_y = r * (brickHeight + brickPadding) + 60;
+			bricks[c][r].x = brick_x;
+			bricks[c][r].y = brick_y;
+
+			bricks[2][2].brickColor = "red";
+			bricks[2][0].brickColor = "green";
+
+			if (bricks[c][r].status == true) {
+				//brick is hit
+				if (
+					ball_y + ball_diameter / 2 >= bricks[c][r].y &&
+					ball_y + ball_diameter / 2 <=
+						bricks[c][r].y + brickHeight &&
+					ball_x >= bricks[c][r].x &&
+					ball_x <= bricks[c][r].x + brickWidth
+				) {
+					bricks[c][r].status = false;
+					brickCount -= 1;
+					if (brickCount == 0) {
+						ball_dy = 0;
+						ball_dx = 0;
+						paddle_dx = 0;
+					}
+					if (bricks[c][r].brickColor == "red") {
+						score -= 2;
+						brickCount += 1;
+					} else if (bricks[c][r].brickColor == "green") {
+						score += 5;
+					} else {
+						score += 1;
+					}
+					ball_dy = -ball_dy;
+				}
+			}
+			//initialise the brick
+			if (bricks[c][r].status == true) {
+				fill(bricks[c][r].brickColor);
+				rect(bricks[c][r].x, bricks[c][r].y, brickWidth, brickHeight);
+				fill(255, 255, 255);
+			}
+		}
 	}
 
-	circle(ball_x, ball_y, ball_diameter);
-	rect(paddle_x, paddle_y, paddle_width, paddle_height);
-	rect(brick_x, brick_y, brick_width, brick_height);
+	//text formatting
+	textSize(14);
+	fill(255, 255, 255);
+	//displaying the score
+	text("Your Score: " + score, 20, 30);
+	if (brickCount == 1) {
+		fill(0, 255, 0);
+		text("Your Score: " + score, 20, 30);
+	}
+	fill(255, 255, 255);
+	//displaying the lives
+	text("Lives Remaining: " + lives, width - 150, 30);
+	if (lives <= 1) {
+		fill(255, 0, 0);
+		text("Lives Remaining: " + lives, width - 150, 30);
+	}
+	fill(255, 255, 255);
+	//text("Lives Remaining: " + lives, width - 150, 30);
+
+	if (lives == 0) {
+		textSize(36);
+		text("Game Over!", 100, 200);
+	}
+	if (brickCount == 0) {
+		textSize(36);
+		text("Wohoo!", 140, 200);
+	}
 }
